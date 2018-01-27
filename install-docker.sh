d7745c894e15e6423d32be40d70f0552fdfbef42
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2018-01-26 19:29:02 -0600

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-cd ~/Documents
-sudo mkdir Docker
-cd Docker
-wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
-sudo dpkg -i docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
-sudo groupadd docker
-which docker
-sudo usermod -aG docker $USER
+exec cd ~/
+exec sudo mkdir Docker
+exec cd Docker
+exec wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
+exec sudo dpkg -i docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
+exec sudo groupadd docker
+exec which docker
+exec sudo usermod -aG docker $USER
 echo "{\"experimental\":true}" > /etc/docker/daemon.json
-cat /etc/docker/daemon.json
-sudo systemctl daemon-reload
-sudo systemctl restart docker
+exec cat /etc/docker/daemon.json
+exec sudo systemctl daemon-reload
+exec sudo systemctl restart docker
