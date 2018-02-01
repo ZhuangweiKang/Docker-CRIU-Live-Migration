90383c27116c34f30f350c4ef0f9665279b452b9
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-01-31 20:54:58 -0600

    Update

---
+++ upgrade-Linux-Kernel.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd /home/ubuntu/
+mkdir Linux-Kernel && cd Linux-Kernel
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14/linux-headers-4.14.0-041400_4.14.0-041400.201711122031_all.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14/linux-headers-4.14.0-041400-generic_4.14.0-041400.201711122031_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14/linux-image-4.14.0-041400-generic_4.14.0-041400.201711122031_amd64.deb
+sudo dpkg -i *.deb
+sudo reboot
