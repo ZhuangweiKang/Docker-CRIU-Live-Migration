332793fc40335389225ee86a4d93ebc7eca89cfc
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-01-31 22:26:48 -0600

    update

--- cloudlet-dependency.sh
+++ cloudlet-dependency.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 sudo apt-get install python-dev
-sudo easy_install pip
 sudo apt-get install python-pip python-dev build-essential
 sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv 
