98d81571af05170cc0a5fc87c88766d7a8c20c81
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-01-31 22:09:48 -0600

    update

--- cloudlet-dependency.sh
+++ cloudlet-dependency.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 sudo apt-get install python-dev
-sudo easy_install pip
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv 
 sudo apt-get install liblz4-tool
 sudo pip install docker-py
 sudo pip install netifaces
